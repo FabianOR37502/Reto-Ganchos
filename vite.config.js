a6848724bf094a3a698e4e05116415bdf0e40f37
FabianOR37502 <ortegafabian370@gmail.com>
2025-07-04 22:29:41 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Reto-Ganchos/', // ¡Nombre exacto de tu repositorio!
 })
